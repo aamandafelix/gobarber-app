328e5e968844e0e3666a3a43b3e20e946ed714c2
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-09-14 10:37:01 -0300

    Setup app basic navigation

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from '../SignUp/styles';
+
+const SignIn: React.FC = () => {
+  function todo() {
+    // todo
+  }
+  return (
+    <Container style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Text>Olar</Text>
+    </Container>
+  );
+};
+
+export default SignIn;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+  function todo() {
+    // todo
+  }
+  return (
+    <Container style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Text>Olar</Text>
+    </Container>
+  );
+};
+
+export default SignUp;
